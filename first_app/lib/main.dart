723404bd556f8171b2f5edade2c828b64ca25f72
JUMTBOX <yjey12@naver.com>
2024-01-29 23:50:05 +0900

    feat. onwards to custom widgets

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,11 +2,28 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Hello Flutter"),
-        ),
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 120, 4, 188),
+                  Color.fromARGB(221, 183, 89, 255),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const Center(
+              child: Text(
+                "Hello Flutter",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                ),
+              ),
+            )),
       ),
     ),
   );
